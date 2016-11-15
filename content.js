1745baf8530481735f4a3ae5f597a49eb9a784d7
blee062 <blee062@ucr.edu>
2016-11-15 10:15:07 -0800

    Trying to debug this

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/Millenials/gi, 'Snake people');
+            var replacedText = text.replace(/Millenials/, 'Snake people');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
